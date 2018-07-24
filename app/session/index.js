6705e794d9f012c901111f87582d67a25bb2c735
Brandon <brandontkessler@gmail.com>
2018-07-23 21:24:55 -0700

    removed connect-mongo

--- app/session/index.js
+++ app/session/index.js
@@ -1,17 +1,13 @@
 'use strict';
 const path = require('path'),
   mongoose = require('mongoose'),
-  MongoStore = require('connect-mongo'),
   config = require(path.join(process.cwd(), 'app', 'config'))
 
 if(process.env.NODE_ENV === 'production') {
   module.exports = {
     secret: config.sessionSecret,
     resave: false,
-    saveUninitialized: true,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection
-    }),
+    saveUninitialized: true
     cookie: {
       httpOnly: true,
       secure: true
