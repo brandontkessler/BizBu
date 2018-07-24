180e8527c0aebd4c10a680e9d66202645c8b7529
Brandon <brandontkessler@gmail.com>
2018-07-23 18:37:02 -0700

    config update

--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -1,9 +1,14 @@
 'use strict';
 const path = require('path'),
   router = require('express').Router(),
-  middleware = require(path.join(process.cwd(), 'app', 'middleware'))
+  middleware = require(path.join(process.cwd(), 'app', 'middleware')),
+  logger = require(path.join(process.cwd(), 'logger'))
 
-let home = (req, res) => res.render('home')
+let home = (req, res) => {
+  logger.log(process.env.HOST)
+  logger.log(process.env.NODE_ENV)
+  res.render('home')
+}
 let getStarted = (req, res) => res.render('home/get-started')
 
 // HOME
