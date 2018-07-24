3f192dbedd3ea068cd01de62f84ab918b8c6566d
Brandon <brandontkessler@gmail.com>
2018-07-24 12:27:15 -0700

    logs for debug

--- app/routes/auth/index.js
+++ app/routes/auth/index.js
@@ -3,7 +3,8 @@ const path = require('path'),
   router = require('express').Router(),
   // { authFB, authFBcbMiddleware, authFBcb,
   { authLI, authLIcbMiddleware, authLIcb, logout } = require(path.join(__dirname, 'social')),
-	middleware = require(path.join(process.cwd(), 'app', 'middleware'))
+	middleware = require(path.join(process.cwd(), 'app', 'middleware')),
+  logger = require(path.join(process.cwd(), 'app', 'logger'))
 
 // FB routes
 // router.get('/auth/facebook', authFB)
@@ -11,7 +12,13 @@ const path = require('path'),
 
 // LinkedIn routes
 router.get('/auth/linkedin', authLI, (req, res) => console.log('failed auth'))
-router.get('/auth/linkedin/callback', authLIcbMiddleware, authLIcb)
+router.get('/auth/linkedin/callback', (req, res, next) => {
+  logger.log('pre middleware')
+  next()
+}, authLIcbMiddleware, (req, res, next) => {
+  logger.log('post middleware')
+  next()
+}, authLIcb)
 
 // LOGOUT
 router.get('/logout', middleware.activeRemoveLogout, logout)
