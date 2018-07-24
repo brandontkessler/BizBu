0a00da2fe7718c8aa0fccd50953658287f3ba210
Brandon <brandontkessler@gmail.com>
2018-07-24 13:55:48 -0700

    added privacy policy

--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -4,10 +4,12 @@ const path = require('path'),
   middleware = require(path.join(process.cwd(), 'app', 'middleware'))
 
 let home = (req, res) => res.render('home')
+let privacyPolicy = (req, res) => res.render('home/privacy-policy')
 let getStarted = (req, res) => res.render('home/get-started')
 
 // HOME
 router.get('/', middleware.activeRemovePageLeave, home)
+router.get('/privacy-policy', privacyPolicy)
 router.get('/get-started', getStarted)
 
 module.exports = router
