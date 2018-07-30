95dcce5d380ea5102ef29f107137f8235f4c8b1c
Brandon <brandontkessler@gmail.com>
2018-07-29 18:26:22 -0700

    logger home route

--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -1,7 +1,8 @@
 'use strict';
 const path = require('path'),
   router = require('express').Router(),
-  middleware = require(path.join(process.cwd(), 'app', 'middleware'))
+  middleware = require(path.join(process.cwd(), 'app', 'middleware')),
+  logger = require(path.join(process.cwd(), 'app', 'logger'))
 
 let home = (req, res) => {
   console.log(process.env.PORT)
