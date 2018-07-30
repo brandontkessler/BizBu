c85e46aa557e725f429a6168e14fb2ba3f52040b
Brandon <brandontkessler@gmail.com>
2018-07-29 18:25:40 -0700

    add port logger

--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -3,7 +3,10 @@ const path = require('path'),
   router = require('express').Router(),
   middleware = require(path.join(process.cwd(), 'app', 'middleware'))
 
-let home = (req, res) => res.render('home')
+let home = (req, res) => {
+  console.log(process.env.PORT)
+  res.render('home')
+}
 let privacyPolicy = (req, res) => res.render('home/privacy-policy')
 
 // HOME
